f7cc3f18a923b2dd3b18f06118d0383fc2f0826b
Daniyar <ensw01119@student.su.edu.krd>
2023-04-12 03:16:35 +0300

    filter

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -6,11 +6,21 @@ import {
   doc,
   updateDoc,
   deleteDoc,
+  onSnapshot,
+  query,
+  where,
 } from "firebase/firestore";
 
 export default function UserList() {
   const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
   const collectionRef = collection(database, "users");
+  const nameQuery = query(
+    collectionRef,
+    where("firstName", ">=", "d"),
+    where("firstName", "<", "e")
+  );
+
   useEffect(() => {
     // ---------  view data from firebase  ---------
     getDocs(collectionRef)
@@ -26,6 +36,16 @@ export default function UserList() {
       });
   }, [collectionRef]);
 
+  // ---------  view data from firebase in real time  ---------
+  const filterData = () => {
+    onSnapshot(nameQuery, (snapshot) => {
+      setFilteredUsers(
+        snapshot.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id };
+        })
+      );
+    });
+  };
   const updateData = (id) => {
     const docToUpdate = doc(database, "users", id);
     updateDoc(docToUpdate, {
@@ -53,51 +73,107 @@ export default function UserList() {
   };
 
   return (
-    <div className="py-12 bg-gray-100 flex flex-col justify-center sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="text-center text-3xl font-extrabold text-gray-900">
-          User List
-        </h2>
-      </div>
+    <>
+      <div className="py-12 bg-gray-100 flex flex-col justify-center sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="text-center text-3xl font-extrabold text-gray-900">
+            User List
+          </h2>
+        </div>
 
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <ul className="border border-gray-200 divide-y divide-gray-200 rounded-md">
-            {users.map((user) => (
-              <li
-                key={user.id}
-                className="pl-3 pr-4 py-3 flex items-center justify-between text-sm"
-              >
-                <div className="w-0 flex-1 flex items-center">
-                  <span className="ml-2 flex-1 w-0 truncate">
-                    {user.firstName}
-                  </span>
-                </div>
-                <div className="ml-4 flex-shrink-0">
-                  <button
-                    onClick={() => {
-                      updateData(user.id);
-                    }}
-                    className="font-medium text-blue-600 hover:text-blue-500"
-                  >
-                    Update
-                  </button>
-                </div>
-                <div className="ml-4 flex-shrink-0">
-                  <button
-                    onClick={() => {
-                      deleteData(user.id);
-                    }}
-                    className="font-medium text-red-600 hover:text-red-500"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <ul className="border border-gray-200 divide-y divide-gray-200 rounded-md">
+              {users.map((user) => (
+                <li
+                  key={user.id}
+                  className="pl-3 pr-4 py-3 flex items-center justify-between text-sm"
+                >
+                  <div className="w-0 flex-1 flex items-center">
+                    <span className="ml-2 flex-1 w-0 truncate">
+                      {user.firstName}
+                    </span>
+                  </div>
+                  <div className="ml-4 flex-shrink-0">
+                    <button
+                      onClick={() => {
+                        updateData(user.id);
+                      }}
+                      className="font-medium text-blue-600 hover:text-blue-500"
+                    >
+                      Update
+                    </button>
+                  </div>
+                  <div className="ml-4 flex-shrink-0">
+                    <button
+                      onClick={() => {
+                        deleteData(user.id);
+                      }}
+                      className="font-medium text-red-600 hover:text-red-500"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <button
+          onClick={() => {
+            filterData();
+          }}
+          className=" mt-5 mx-auto block py-2 px-5 rounded bg-yellow-500 hover:bg-amber-950 font-medium text-yellow-900 hover:text-yellow-500"
+        >
+          Filter
+        </button>
+      </div>
+      <div className="py-12 bg-gray-100 flex flex-col justify-center sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="text-center text-3xl font-extrabold text-gray-900">
+            Filtered User List
+          </h2>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <ul className="border border-gray-200 divide-y divide-gray-200 rounded-md">
+              {filteredUsers.map((user) => (
+                <li
+                  key={user.id}
+                  className="pl-3 pr-4 py-3 flex items-center justify-between text-sm"
+                >
+                  <div className="w-0 flex-1 flex items-center">
+                    <span className="ml-2 flex-1 w-0 truncate">
+                      {user.firstName}
+                    </span>
+                  </div>
+                  <div className="ml-4 flex-shrink-0">
+                    <button
+                      onClick={() => {
+                        updateData(user.id);
+                      }}
+                      className="font-medium text-blue-600 hover:text-blue-500"
+                    >
+                      Update
+                    </button>
+                  </div>
+                  <div className="ml-4 flex-shrink-0">
+                    <button
+                      onClick={() => {
+                        deleteData(user.id);
+                      }}
+                      className="font-medium text-red-600 hover:text-red-500"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
